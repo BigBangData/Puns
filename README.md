77b3bab6f2f85818e814047d7fc690a367ccbbac
Marcelo Sanches <sanches.cello@gmail.com>
2023-12-29 16:06:09 -0700

    Fix a few more issues before Tuturial 9

--- README.md
+++ README.md
@@ -8,16 +8,15 @@ Sourced puns from https://wstyler.ucsd.edu/puns/, among others.
 Tutorials: https://www.youtube.com/watch?v=9MHYHgh4jYc&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=6
 Bootstrap: https://getbootstrap.com/docs/5.3/getting-started/introduction/
 
-On Tutorial 8:
-    - many issues solved
-    - a logged out user can still "log out successfully"
-    - assumes name doesn't repeat in database
-    - need to create an admin login
-    - users can only delete themselves but message doesn't show in view
-    - might want to create separate pages for viewing/deleting?
-    VIEWING
-    - as admin, see all users
-    - as user, only see yourself
-    DELETING
-    - as admin, delete any user
-    - as user, delete yourself
+Solved:
+    - a logged out user can log out, doesn't display 'successful logout' if no one is logged in
+    - a user can only view and delete their own entry in the database
+    - added logs and logging info to debug issues and see in realtime what is gonig on
+
+Issues:
+    - assumes name doesn't repeat in database, so no two different entires for the same name can exist
+    - need to create an admin login either as separate page or as separate user in the view func
+    - admin should be able to view all users (right now we can't)
+    - create separate form to delete email
+    - maybe logout user if user is deleted
+    - ideally ask to create account instead of just creating one
--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import os
-from datetime import timedelta
+import logging
+from datetime import datetime, timedelta
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
+# setup logging for file and console
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+log_file = os.path.join('logs', f"{datetime.now().strftime('%Y%m%d_%H%M%SMT')}.log")
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename=log_file, level=logging.INFO, format=log_format)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter(log_format)
+console_handler.setFormatter(formatter)
+
+# setup flask app
 app = Flask(__name__)
 app.secret_key = "wjer3498$&_VKA3lkf=="
 # store session data for 5 min; works even if browser is open
@@ -44,16 +57,23 @@ def login():
             # if user exists, grab email for session
             session["email"] = found_user.email
         else:
+            msg = f"Creating user {user} in database."
+            logging.info(msg=msg)
             usr = users(user, "") # leave email blank
             db.session.add(usr) # stage
             # doesn't add automatically because one could rollback
             db.session.commit()
-        
-        flash(f"Login Successful!", "info")
+        # display message
+        msg = f"{user}, you are logged in."
+        flash(msg, "info")
+        logging.info(msg=msg)
         return redirect(url_for("user"))
     else:
         if "user" in session:
-            flash(f"Already logged in.", "info")
+            logged_user = session["user"]
+            msg = f"{logged_user}, you are already logged in."
+            flash(msg, "info")
+            logging.info(msg=msg)
             return redirect(url_for("user"))
         else:
             return render_template('login.html')
@@ -62,29 +82,43 @@ def login():
 def user():
     email = None
     if "user" in session:
-        user = session["user"]
+        logged_user = session["user"]
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
-            found_user = users.query.filter_by(name=user).first()
+            found_user = users.query.filter_by(name=logged_user).first()
             found_user.email = email
             db.session.commit()
-            flash("Email was saved.", "info")
+            msg = f"{logged_user}, your email {email} was saved."
+            flash(msg, "info")
+            logging.info(msg=msg)
         else:
             if "email" in session:
                 email = session["email"]
         return render_template("user.html", email=email)
     else:
-        flash("You're not logged in.", "info")
+        msg = f"{user}, you are already logged in."
+        flash(msg, "info")
+        logging.info(msg=msg)
         return redirect(url_for("login"))
 
 @app.route('/logout')
 def logout():
-    flash("You've logged out successfully", "info")
-    # clear out user data from session
-    session.pop("user", None)
-    session.pop("email", None)
-    return redirect(url_for("login"))
+    if "user" in session:
+        logged_user = session["user"]
+        msg = f"{logged_user}, you have logged out successfully."
+        flash(msg, "info")
+        logging.info(msg=msg)
+        session.pop("user", None)
+        session.pop("email", None)
+        return redirect(url_for("login"))
+    else:
+        msg = "You are not logged in."
+        flash(msg, "info")
+        logging.info(msg=msg)
+        session.pop("user", None)
+        session.pop("email", None)
+        return redirect(url_for("login"))
 
 @app.route('/view', methods=["POST", "GET"])
 def view():
@@ -96,21 +130,41 @@ def view():
             if logged_user == requested_user:
                 users.query.filter_by(name=logged_user).delete()
                 db.session.commit()
-                # why is this showing in login.html not view.html?
-                flash(f"User {requested_user} deleted.", "info")
+                msg = f"{logged_user} successfully deleted."
+                flash(msg, "info")
+                logging.info(msg=msg)
+                # since we can have multiple users with the same name
+                same_name_users = users.query.filter_by(name=logged_user).all()
+                return render_template("view.html", values=same_name_users)
             else:
-                flash(f"User {logged_user} not authorized to delete {requested_user}.", "info")
-    
-    return render_template("view.html", values=users.query.all())
+                ui_msg = f"User  {logged_user} not authorized to delete requested user."
+                console_msg = f"User {logged_user} not authorized to delete {requested_user}."
+                flash(ui_msg, "info")
+                logging.info(msg=console_msg)
+                same_name_users = users.query.filter_by(name=logged_user).all()
+                return render_template("view.html", values=same_name_users)
+        elif request.method == "GET":
+            same_name_users = users.query.filter_by(name=logged_user).all()
+            return render_template("view.html", values=same_name_users)
+        else:
+            pass
+    else:
+        flash(f"Please log in to view and manage user data.", "info")
+        return redirect(url_for("login"))
 
 # @app.route('/admin')
 # def admin():
-#     return redirect(url_for("user", name="Admin"))
+#     return redirect(url_for("user", name="admin"))
 
 if __name__ == '__main__':
+    # add the console handler to the root logger
+    logging.getLogger('').addHandler(console_handler)
+    # access db
     with app.app_context():
         db.create_all()
-    
+    # run app
     app.run(debug=True)
     # app.run(host='0.0.0.0', port=5000) # to run
-
+    # close the console handler to avoid resource leaks
+    console_handler.close()
+    logging.getLogger('').removeHandler(console_handler)
