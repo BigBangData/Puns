2cbae94cd1e53056723d1329ce6bddfdea1b97b4
Marcelo Sanches <sanches.cello@gmail.com>
2023-12-31 21:11:26 -0700

    Revised webpage upto pivot to puns

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 # Puns
 
 Credits:
-- sourced puns from https://wstyler.ucsd.edu/puns/, among others.
-- `curated_puns.txt`: manually curated text file that serves as input to create the database; in question?answer format
-
-Tutorials:
-- followed: https://www.youtube.com/watch?v=71EU8gnZqZQ
-    - GitHub: https://github.com/arpanneupane19/Python-Flask-Authentication-Tutorial/blob/main/app.py
-- did not follow Tim's: https://www.youtube.com/watch?v=W4GItcW7W-U 
-    - GitHub: https://github.com/techwithtim/Flask-Blog-Tutorial
-- just read directly from `curated_puns.txt` no need to create a database for puns
+- sourced puns from https://wstyler.ucsd.edu/puns/, among other random from memory.
 
 Notes:
 - database is for users so far, and any users can create accounts at any time, only usernames and passwords
@@ -22,35 +14,39 @@ sqlite> select * from user;
 sqlite> .exit
 ```
 
-Next Steps:
-- test going from login to view to home etc and think through various flows again to flash messages
-    + it's a bit of a hybrid of tutorials, so look at commented out code for user flows
-- right now functionality is very limited and logout only exists in `view.html`
-- recreate `view.html` to be a dashboard for puns and not a user management page
+Functionality:
+- signup is always possible, even for logged in users (can signup as another user, but not login)
+- signup username must be unique
+- login only possible if not currently logged in, throws message and redirects to view
+- logout only possible in view
+- view is the ultimate goal, where the "game" happens
+
+Todo:
+- change view to the pun game
+- `curated_puns.txt`: manually curated text file that serves as input to create the database; in question?answer format
+- just read directly from `curated_puns.txt` no need to create a database for puns
+
+Final Steps:
+- deploy to ubuntu server
+- follow Tim's last tutorial: https://www.youtube.com/watch?v=YFBRVJPhDGY&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=11
 
 ---
 
-## Old Notes
+## Tutorials
 
-Tutorials from Tim Ruscica: https://www.youtube.com/@TechWithTim, https://www.techwithtim.net/, https://github.com/techwithtim
+Tutorials from Tim Ruscica: 
+- [Flask Series on YouTube](https://www.youtube.com/@TechWithTim)
+- [Website](https://www.techwithtim.net)
+- [Github](https://github.com/techwithtim)
 
-Bootstrap: https://getbootstrap.com/docs/5.3/getting-started/introduction/
+Tutorial from Arpan Neupane:
+- [Authentication Video on YouTube](https://www.youtube.com/watch?v=71EU8gnZqZQ)
+- [GitHub](https://github.com/arpanneupane19/Python-Flask-Authentication-Tutorial/blob/main/app.py)
+
+More Links:
+- [Bootstrap Intro](https://getbootstrap.com/docs/5.3/getting-started/introduction/)
 
 Blueprint tutorial notes:
 - a way to structure entire project and package it so it's usable in other projects
 - so for example could create admin folder and in it add templates, static, etc.,
 - then could import from admin by packaging folder (create __init__.py empty file inside folder)
-- not going to use this knowledge for this project, focus on deliverable
-
-Solved:
-- a logged out user can log out, doesn't display 'successful logout' if no one is logged in
-- a user can only view and delete their own entry in the database
-- added logs and logging info to debug issues and see in realtime what is going on
-
-Issues:
-- assumes name doesn't repeat in database, so no two different entires for the same name can exist
-- need to create an admin login either as separate page or as separate user in the view func
-- admin should be able to view all users (right now we can't)
-- create separate form to delete email
-- maybe logout user if user is deleted
-- ideally ask to create account instead of just creating one
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from datetime import datetime, timedelta
-from flask import Flask, redirect, url_for, render_template, flash, request, session
+from flask import Flask, redirect, url_for, render_template, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -22,7 +22,6 @@ console_handler.setFormatter(formatter)
 
 # setup flask app
 app = Flask(__name__)
-app.secret_key = "wjer3498$&_VKA3lkf=="
 # store session data for 5 min; works even if browser is open
 app.permanent_session_lifetime = timedelta(minutes=5)
 
@@ -43,7 +42,6 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
@@ -62,13 +60,12 @@ class RegisterForm(FlaskForm):
         validators=[InputRequired(), Length(min=4, max=20)]
         , render_kw={"placeholder": "Password"}
     )
-    submit = SubmitField("Register")
+    submit = SubmitField("Signup")
 
     def validate_username(self, username):
-        existing_username = User.query.filter_by(username=username.data).first()
-        if existing_username:
-            msg = 'Username already exists. Please try another.'
-            raise ValidationError(msg)
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError("User exists.")
 
 class LoginForm(FlaskForm):
     username = StringField(
@@ -81,174 +78,91 @@ class LoginForm(FlaskForm):
     )
     submit = SubmitField("Login")
 
-
+# home
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/register", methods=["POST", "GET"])
-def register():
+# signup
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
     form = RegisterForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            msg = f"Created new account for {form.username.data}. Please login."
+            logging.info(msg=msg)
+            flash(msg, "info")
+            return redirect(url_for('login'))
+        else:
+            msg = f"Username '{form.username.data}' exists. Please try another."
+            logging.info(msg=msg)
+            flash(msg, "info")
+            return render_template("signup.html", form=form)
+    # "GET"
+    else:
+        return render_template("signup.html", form=form)
 
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-
-    return render_template("register.html", form=form)
-
+# login
 @app.route("/login", methods=["POST", "GET"])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        user_id = User.query.filter_by(username=form.username.data).first()
-        if user_id:
-            if bcrypt.check_password_hash(user_id.password, form.password.data):
-                login_user(user_id)
-                msg = f"Hello {user_id.username}, you are logged in."
-                flash(msg, "info")
-                return redirect(url_for('view'))
-    return render_template('login.html', form=form)
+    if request.method == "GET":
+        # cannot do "if current_user:"
+        # will have AnonymousUserMixin with no .username
+        try:
+            msg = f"{current_user.username}, you're already logged in."
+            flash(msg, "info")
+            return redirect(url_for('view'))
+        # throws error when current_user is Anonymous (not logged in)
+        except AttributeError:
+            return render_template('login.html', form=form)
+    # "POST"
+    else:
+        if form.validate_on_submit():
+            user_id = User.query.filter_by(username=form.username.data).first()
+            if user_id:
+                if bcrypt.check_password_hash(user_id.password, form.password.data):
+                    login_user(user_id)
+                    flash(f"Hello {user_id.username}, you are logged in.", "info")
+                    return redirect(url_for('view'))
+                else:
+                    logging.info(msg=f"Wrong password for {form.username.data}.")
+                    flash("Wrong password.", "info")
+                    return render_template('login.html', form=form)
+            else:
+                flash("Did you signup for an account yet?")
+                return redirect(url_for('signup'))
 
-# def register():
-#     if request.method == "POST":
-#         # start session
-#         session.permanent = True
-#         # get username
-#         username = request.form["username"]
-#         session["username"] = username
-#         # check if user already exists
-#         # if so, redirect to login
-#         found_user = User.query.filter_by(username=username).first()
-#         if found_user:
-#             msg = f"{username}, you already have an account. Please login." # check if logged in already
-#             flash(msg, "info")
-#             logging.info(msg=msg)
-#             return redirect(url_for("login")) # gets two messages if already logged in
-#         else:
-#             # create an account
-#             internal_msg = f"Creating user {username} in database."
-#             logging.info(msg=internal_msg)
-#             usr = User(username, "")
-#             db.session.add(usr) # stage
-#             # doesn't add automatically because one could rollback
-#             db.session.commit()
-#             # message user
-#             msg = f"Created account for {username}."
-#             logging.info(msg=msg)
-#             usr = User(username, "")
-#             return redirect(url_for("login")) # gets two messages if already logged in
-#     # "GET" method
-#     else:
-#         return render_template('register.html')
-
-# def login():
-#     if request.method == "POST":
-#         # start session
-#         session.permanent = True
-#         # get username
-#         username = request.form["username"]
-#         session["username"] = username
-#         # check if user already exists
-#         # find all users with this name, grab first
-#         # assumes names are unique, coult try .all() or .delete()
-#         # could loop through objects if using .all() to delete them
-#         found_user = User.query.filter_by(username=username).first()
-#         if found_user:
-#             # if user exists, grab password for session
-#             session["password"] = found_user.password
-#             # authenticate first
-#             msg = f"{username}, you are logged in."
-#             flash(msg, "info")
-#             logging.info(msg=msg)
-#             return redirect(url_for("view"))
-#         else:
-#             # redirect to register
-#             msg = f"User {username} not found. Please register for an account."
-#             logging.info(msg=msg)
-#             flash(msg, "info")
-#             return redirect(url_for("register"))
-#     # "GET" method
-#     else:
-#         if "username" in session:
-#             logged_user = session["username"]
-#             msg = f"{logged_user}, you are already logged in."
-#             flash(msg, "info")
-#             logging.info(msg=msg)
-#             return redirect(url_for("view"))
-#         else:
-#             return render_template('login.html')
-
-
+# logout
 @app.route('/logout', methods=["POST", "GET"])
 @login_required
 def logout():
     logout_user()
+    flash("You've successfully logged out.")
     return redirect(url_for('login'))
-    # if "username" in session:
-    #     logged_user = session["username"]
-    #     msg = f"{logged_user}, you have logged out successfully."
-    #     flash(msg, "info")
-    #     logging.info(msg=msg)
-    #     session.pop("username", None)
-    #     session.pop("password", None)
-    #     return redirect(url_for("login"))
-    # else:
-    #     msg = "You are not logged in."
-    #     flash(msg, "info")
-    #     logging.info(msg=msg)
-    #     return redirect(url_for("login"))
 
-
+# view
 @app.route('/view', methods=["POST", "GET"])
 @login_required
 def view():
+    #user_filter = User.query.filter_by(username=current_user.username).first()
     admin_filter = User.query.all()
     return render_template('view.html', values=admin_filter)
-    # # only user can delete their data
-    # if "username" in session:
-    #     logged_user = session["username"]
-    #     if request.method == "POST":
-    #         requested_user = request.form["username"]
-    #         if logged_user == requested_user:
-    #             # permission to delete its own data
-    #             User.query.filter_by(username=logged_user).delete()
-    #             db.session.commit()
-    #             msg = f"{logged_user} successfully deleted."
-    #             flash(msg, "info")
-    #             logging.info(msg=msg)
-    #             # user filter
-    #             # user_filter = User.query.filter_by(username=logged_user).all()
-    #             # revamped permission to view all users
-    #             admin_filter = User.query.all()
-    #             return render_template("view.html", values=admin_filter)
-    #         else:
-    #             ui_msg = f"User {logged_user} not authorized to delete requested user."
-    #             console_msg = f"User {logged_user} not authorized to delete {requested_user}."
-    #             flash(ui_msg, "info")
-    #             logging.info(msg=console_msg)
-    #             admin_filter = User.query.all()
-    #             return render_template("view.html", values=admin_filter)
-    #     # "GET" method
-    #     else:
-    #         admin_filter = User.query.all()
-    #         return render_template("view.html", values=admin_filter)
-    # else:
-    #     flash(f"Please log in to view and manage user data.", "info")
-    #     return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
     # add the console handler to the root logger
     logging.getLogger('').addHandler(console_handler)
-    # access db
+    # access database
     with app.app_context():
         db.create_all()
     # run app
     app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000) # to run
+    # app.run(host='0.0.0.0', port=5000) # to run in prod
     # close the console handler to avoid resource leaks
     console_handler.close()
     logging.getLogger('').removeHandler(console_handler)
